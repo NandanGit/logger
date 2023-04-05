9ee2baf8b549ab14d98d7b56456a5ff5444dbd38
Nandan Reddy <nandan.reddy1133@gmail.com>
2023-04-05 20:39:35 +0530

    Added Prettier
    Added prettier configuration to the project in order to maintain a uniform formatting
    
    Co-Authored-By: Nandan <57260750+nandangit@users.noreply.github.com>

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 export const COLOR_MAP = {
-	default: '\x1b[0m',
-	success: '\x1b[32m',
-	info: '\x1b[34m',
-	warning: '\x1b[33m',
-	error: '\x1b[31m',
-	debug: '\x1b[35m',
+  default: '\x1b[0m',
+  success: '\x1b[32m',
+  info: '\x1b[34m',
+  warning: '\x1b[33m',
+  error: '\x1b[31m',
+  debug: '\x1b[35m',
 };
